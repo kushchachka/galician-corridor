8c9a79392c10cbe0b14d38d70b342d9fbf0312bf
kushchachka <yana200864@gmail.com>
2026-03-25 11:20:34 +0200

    Added attack animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
+var is_attacking = false
 
 func _physics_process(delta):
 
@@ -13,12 +14,21 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		is_attacking = true
+		sprite.play("attack")
+
+	if is_attacking:
+		velocity.x = 0
+		move_and_slide()
+		return
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 
 	if direction != 0:
 		velocity.x = direction * SPEED
-		sprite.play("walked")
+		sprite.play("walk")
 
 		if direction < 0:
 			sprite.flip_h = true
@@ -29,3 +39,8 @@ func _physics_process(delta):
 		sprite.play("idle")
 
 	move_and_slide()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation == "attack":
+		is_attacking = false
+		sprite.play("idle")
